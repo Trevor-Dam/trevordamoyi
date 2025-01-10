7b718f55d4a0ee9a94d613f4f9023ca8c67591fb
Trevor Damoyi <trevordamoyi307@gmail.com>
2025-01-10 16:52:29 +0200

    Initial Commit

---
+++ src/main/java/com/enviro/assessment/grad001/trevordamoyi/TrevordamoyiApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.trevordamoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrevordamoyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrevordamoyiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/trevordamoyi/TrevordamoyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.trevordamoyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrevordamoyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
